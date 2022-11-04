5b2e24bf4d836f917b5800c53b5097365429e856
Jessica <phyusin.21@ichat.sp.edu.sg>
2022-11-04 15:18:24 +0800

    Sent successfully

---
+++ bmi.py
@@ -0,0 +1,22 @@
+def get_user_input():
+ x = input()
+ y=x.split()
+ return y
+def display_main_menu():
+ print("display_main_menu")
+
+def main():
+ get_user_input()
+ display_main_menu()
+ num_list = get_user_input()
+
+def display_main_menu():
+ print("display_main_menu")
+
+ def calc_average():
+     print("calc_average")
+
+
+
+
+
